5a78610083c98ef4c16770f490dff8bc47757734
satsheelshelke-web <satsheelshelke@gmail.com>
2026-06-18 17:08:39 +0530

    first python code

---
+++ myinfo.py
@@ -0,0 +1,10 @@
+name= "Student"
+age=17
+city="Maharashtra"  
+print("My name is ",name)
+print("I am ",age,"years old")
+print("I live in ",city)
+college= "Engineering college"
+branch="Robotics and AI"
+print("College:",college)
+print("Branch:",branch)
