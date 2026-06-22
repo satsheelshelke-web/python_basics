bf656c64c52404623f365b04f0eaf9af1253b5f1
satsheelshelke-web <satsheelshelke@gmail.com>
2026-06-22 20:11:50 +0530

    Add files via upload

---
+++ Multiplication Table Generator.py
@@ -0,0 +1,9 @@
+print('Multiplication Table Generator')
+
+print('')
+print('Enter number you want to make table of below')
+num=int(input('Num:'))
+i=0
+while i<10:
+    i+=1
+    print(num,'x',i,'=',i*num)
---
+++ age-calculater.py
@@ -0,0 +1,6 @@
+print('   Age Calculator')
+age=int(input('Enter age'))
+x=int(input('After how many years:'))
+
+print('Your age after',x ,'years will be',age+x,'years')      
+      
